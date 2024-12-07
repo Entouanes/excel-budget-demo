b0da4d84a744d7e8c3dede16b4b74d8c00171c95
Entouanes <a.combremont@bluewin.ch>
2024-12-07 15:15:17 +0100

    progress

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,4 +3,14 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="navbar bg-base-200 p-3 shadow-lg">
+	<div class="navbar-start">
+		<h1>SummaRise</h1>
+	</div>
+	<div class="navbar-end">
+		<a href="/about" class="btn btn-ghost">About</a>
+	</div>
+</div>
+<div class="container mx-auto flex justify-center gap-10 h-[650px]">
+	{@render children()}
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="card bg-base-200 w-96" >
+    <h1>Welcome to SvelteKit</h1>
+</div>
+<div>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
